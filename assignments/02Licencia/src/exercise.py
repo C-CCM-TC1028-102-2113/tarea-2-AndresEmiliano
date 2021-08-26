11c8bbde28d7579704adeb5e8888c3197012b599
AndresEmiliano <89090646+AndresEmiliano@users.noreply.github.com>
2021-08-25 23:51:44 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,8 +1,19 @@
 
 def main():
     #Escribe tu código debajo de esta línea
-    pass
+def main():
+    a = int(input("dame tu edad:"))
+    if a>18:
+       print("continua")
+    elif a<18:
+       print("licendia denegada")
+    b = str(input("¿tienes identificacion oficial?"))
+    if b == "s":
+       print("tramite de licencia concedido")
+    elif b == "n":
+       print("tramite de licencia concedido")
 
+main()
 
 if __name__ == '__main__':
     main()
